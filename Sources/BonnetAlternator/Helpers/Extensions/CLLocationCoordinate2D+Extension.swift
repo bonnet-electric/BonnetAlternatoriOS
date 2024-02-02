45731c753e8b3c18b820c851eb350e5a3c7c3086
Ana <ana@joinbonnet.com>
2024-02-02 16:11:33 +0000

    Adding extension for CLLocationCoordinate2D to be codable

--- Sources/BonnetAlternator/Helpers/Extensions/CLLocationCoordinate2D+Extension.swift
+++ Sources/BonnetAlternator/Helpers/Extensions/CLLocationCoordinate2D+Extension.swift
@@ -15,3 +15,35 @@ extension CLLocationCoordinate2D {
         return from.distance(from: to)
     }
 }
+
+extension CLLocationCoordinate2D: Codable, Hashable, Equatable {
+    // MARK: - Hashable
+    public func hash(into hasher: inout Hasher) {
+        hasher.combine(latitude + longitude)
+    }
+    
+    // MARK: - Equatable
+    static public func == (lhs: CLLocationCoordinate2D, rhs: CLLocationCoordinate2D) -> Bool {
+        lhs.latitude == rhs.latitude && lhs.longitude == rhs.longitude
+    }
+    
+    // MARK: - Codable
+    enum CodingKeys: String, CodingKey {
+      case latitude, longitude
+    }
+    
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        
+        let latitudeValue = try container.decode(Double.self, forKey: .latitude)
+        let longitudeValue = try container.decode(Double.self, forKey: .longitude)
+        
+        self.init(latitude: latitudeValue, longitude: longitudeValue)
+    }
+    
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(latitude, forKey: .latitude)
+        try container.encode(longitude, forKey: .longitude)
+    }
+}
